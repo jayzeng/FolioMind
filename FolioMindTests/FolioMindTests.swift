e4a4fea7cf920193f29fc6c7ada7a282a23a7c76
Jay Zeng <jayzeng@users.noreply.github.com>
2025-11-23 15:28:02 -0800

    Initial Commit

---
+++ FolioMindTests/FolioMindTests.swift
@@ -0,0 +1,17 @@
+//
+//  FolioMindTests.swift
+//  FolioMindTests
+//
+//  Created by Jay Zeng on 11/23/25.
+//
+
+import Testing
+@testable import FolioMind
+
+struct FolioMindTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
